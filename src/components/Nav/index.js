58d8189f9702cc946b5a523a96c17a14cb8f7d5d
Paul Hine <paul.hine@gmail.com>
2017-12-21 12:01:35 -0500

    post formatting

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,25 +1,44 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, withRouter, matchPath } from 'react-router-dom'
 import { connect } from 'react-redux'
 import cx from 'classnames'
 import styles from './styles.scss'
 
-const Nav = ({ favorites }) =>
+const data = [
+  {
+    path: '/feed',
+    iconClass: cx(styles.fab, styles['fa-reddit-alien']),
+    label: '/r/analog',
+  },
+  {
+    path: '/favorites',
+    iconClass: cx(styles.fas, styles['fa-heart']),
+    label: 'favorites',
+    renderBadge: ({ favorites }) =>
+      favorites.isLoaded &&
+      <span>
+        ({favorites.isEmpty ? '0' : Object.keys(favorites.dict).length})
+      </span>,
+  },
+]
+
+const Nav = ({ favorites, location }) =>
   <div className={styles.nav}>
     <nav className={styles.bar}>
-      <Link to={'/feed'}>
-        <i className={cx(styles.fab, styles['fa-reddit-alien'])} />
-        Feed
-      </Link>
-      <Link to={'/favorites'}>
-        <i className={cx(styles.fas, styles['fa-heart'])} />
-        Favorites
-        {favorites.isLoaded &&
-          <span>
-            ({favorites.isEmpty ? '0' : Object.keys(favorites.dict).length})
-          </span>}
-      </Link>
+      {data.map(item =>
+        <Link
+          to={item.path}
+          key={item.path}
+          className={cx({ [styles.isActive]: Boolean(matchPath(location.pathname, item.path)) })}
+        >
+          <div className={styles.content}>
+            <i className={item.iconClass} />
+            {item.label}
+            {item.renderBadge && item.renderBadge({ favorites })}
+          </div>
+        </Link>
+      )}
     </nav>
   </div>
 
-export default connect(state => state)(Nav)
+export default withRouter(connect(state => state)(Nav))
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import cx from 'classnames'
+import moment from 'moment'
 import actions from '../../store/favorites/actions'
 import styles from './styles.scss'
 
 const Post = ({ data }) =>
   <article className={styles.post}>
-    {data.title}
+    <div className={styles.photo} style={{ backgroundImage: `url(${data.url})` }} />
+    <div className={styles.title}>
+      {data.title}
+    </div>
+    <div className={styles.stats}>
+      <span>
+        <i className={cx(styles.fas, styles['fa-user'])} />
+        u/{data.author}
+      </span>
+      <span>
+        <i className={cx(styles.fa, styles['fa-clock'])} />
+        {moment.utc(data.created_utc).fromNow()}
+      </span>
+      <span>
+        <i className={cx(styles.fas, styles['fa-bolt'])} />
+        {data.ups}
+      </span>
+    </div>
   </article>
 
 export default connect(null, actions)(Post)
